b9a890feb10a8e36a652f9fb6d9efa08fe04e01b
Ajith Sritharan <sritharanajith04@gmail.com>
2024-02-25 14:48:56 +0530

    25.02.2024 2:48 PM

---
+++ loop practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count =0;
+            Console.WriteLine("Enter a number : ");
+            int n = int.Parse(Console.ReadLine());
+            while (count <= n)
+            {
+                Console.WriteLine("Number : " + count);
+                count++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
