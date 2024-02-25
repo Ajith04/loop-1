3ba180231e34592ec159d686ed80cf85a473e437
Ajith Sritharan <sritharanajith04@gmail.com>
2024-02-25 16:21:50 +0530

    last

--- loop practice/Program.cs
+++ loop practice/Program.cs
@@ -11,6 +11,8 @@ namespace loop_practice
     {
         static void Main(string[] args)
         {
+            //print until given number from 1 using while loop
+
             /*int x =1;
             Console.WriteLine("Enter a number : ");
             int n1 = int.Parse(Console.ReadLine());
@@ -21,6 +23,7 @@ namespace loop_practice
             }
             Console.ReadLine();*/
 
+            // print odd and even numbers seperately from 1 to give number
 
             /*Console.WriteLine("Enter a number : ");
             int n2 = int.Parse(Console.ReadLine());
@@ -91,22 +94,53 @@ namespace loop_practice
 
 
 
+            //find the given number is prime number or not
 
-            Console.Write("Enter a positive number : ");
-            int num = int.Parse(Console.ReadLine());
-            int divider = 2;
-            int maxDivider = (int)Math.Sqrt(num);
-            bool prime = true;
-            while (prime && (divider <= maxDivider))
-            {
-                if (num % divider ==0)
-                {
-                    prime = false;
-                }
-                divider++;
-            }
-            Console.WriteLine("Prime? " + prime);
-            Console.ReadLine();
+            //Console.Write("Enter a positive number : ");
+            //int num = int.Parse(Console.ReadLine());
+            //int divider = 2;
+            //int maxDivider = (int)Math.Sqrt(num);
+            //bool prime = true;
+            //while (prime && (divider <= maxDivider))
+            //{
+            //    if (num % divider ==0)
+            //    {
+            //        prime = false;
+            //    }
+            //    divider++;
+            //}
+            //Console.WriteLine("Prime? " + prime);
+            //Console.ReadLine();
+
+            // find the factorial value of the given number
+
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //decimal factorial = 1;
+            //do
+            //{
+            //    factorial *= n;
+            //    n--;
+            //}
+            //while (n > 0);
+            //Console.WriteLine("n!= " + factorial);
+
+
+            //find factorial within the given two numbers
+            //Console.Write("n= ");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.Write("m= ");
+            //int m = int.Parse(Console.ReadLine());
+            //int num = n;
+            //long product = 1;
+            //do
+            //{
+            //    product *= num;
+            //    num++;
+            //}
+            //while (num <= m);
+            //Console.WriteLine("product[n.....m]" + product);
+
 
 
         }
