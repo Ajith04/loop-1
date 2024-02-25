10834352362b02e22fa9a3c24d4d18847bd24d0d
Ajith Sritharan <sritharanajith04@gmail.com>
2024-02-25 16:02:53 +0530

    4:02 PM

--- loop practice/Program.cs
+++ loop practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,8 +58,55 @@ namespace loop_practice
             Console.ReadLine();*/
 
 
+            /*int sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number : ");
+                int n3 = int.Parse(Console.ReadLine());
+                if (n3 < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    sum += n3;
+                }
+            }
+            Console.Write("The total number is : " + sum);
+            Console.ReadLine();*/
 
 
+            //Console.Write("Enter a number : ");
+            //int nn = int .Parse(Console.ReadLine());
+            //int sum = 0;
+            //while (nn >=0)
+            //{
+            //    sum += nn;
+            //    Console.Write("Enter a number : ");
+            //    nn = int .Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("The total number : " + sum);
+
+
+
+
+
+            Console.Write("Enter a positive number : ");
+            int num = int.Parse(Console.ReadLine());
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(num);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (num % divider ==0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? " + prime);
+            Console.ReadLine();
 
 
         }
