bcd658fd897ebcde7fbb52196c664a4f3a58d28f
Ajith Sritharan <sritharanajith04@gmail.com>
2024-02-25 15:24:07 +0530

    3:24 PM

--- loop practice/Program.cs
+++ loop practice/Program.cs
@@ -10,15 +10,57 @@ namespace loop_practice
     {
         static void Main(string[] args)
         {
-            int count =0;
+            /*int x =1;
             Console.WriteLine("Enter a number : ");
-            int n = int.Parse(Console.ReadLine());
-            while (count <= n)
+            int n1 = int.Parse(Console.ReadLine());
+            while (x <= n1)
             {
-                Console.WriteLine("Number : " + count);
-                count++;
+                Console.WriteLine("Number : " + x);
+                x++;
             }
-            Console.ReadLine();
+            Console.ReadLine();*/
+
+
+            /*Console.WriteLine("Enter a number : ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Odd\t\tEven");
+            int y = 1;
+            while (y<=n2)
+            {
+                if (y%2 == 1)
+                {
+                    Console.WriteLine(y + "\t\t");
+
+                }
+                else
+                {
+                    Console.WriteLine(y);
+                }
+                y++;
+            }*/
+
+
+
+            /*Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 1;
+            Console.Write("The sum 1");
+            while (num < n)
+            {
+                num++;
+                sum += num;
+                Console.Write(" + " + num);
+
+            }
+            Console.Write(" = " + sum);
+            Console.ReadLine();*/
+
+
+
+
+
+
         }
     }
 }
